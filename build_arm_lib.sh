790ed95d1dac646e85f108fac19180932dd5076e
grf123 <guorongfeng@excean.com>
2018-04-25 11:07:03 +0800

    Fix output lib is not for ARM.

--- build_arm_lib.sh
+++ build_arm_lib.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
-CPPFLAGS="-DANDROID" ./configure --host=arm-linux-androideabi --prefix=`pwd`/android/arm
+CPPFLAGS="-DANDROID" ./configure cc=arm-linux-androideabi-gcc --host=arm-linux-androideabi --prefix=`pwd`/android/arm
 make
 ls .libs
 
+#./configure CC=arm-linux-androideabi-gcc --host=arm-linux-androideabi CFLAGS='-march=armv7-a -mfloat-abi=softfp' LDFLAGS='-Wl,--fix-cortex-a8'
